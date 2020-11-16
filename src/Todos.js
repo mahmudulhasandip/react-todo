6eeb0af8947720ed2e2c6d5eafb5e1680dd85359
MH Dip <mhdip.pro@gmail.com>
2020-11-16 15:54:48 +0600

    first commit

---
+++ src/Todos.js
@@ -0,0 +1,26 @@
+
+function Todos({todos, deleteTodo}) {
+
+    const todoList = todos.length ?
+        (
+            todos.map(todo => {
+                return (
+                    <div className="collection-item" key={todo.id}>
+                        <span className="pointer" onClick={()=>{deleteTodo(todo.id)}}>{todo.content}</span>
+                    </div>
+                )
+            })
+        ) 
+        :
+        (
+            <p className="center">You have no todo's left</p>
+        );
+
+    return (
+        <div className="todos collection">
+            {todoList}
+        </div>
+    )
+}
+
+export default Todos;
